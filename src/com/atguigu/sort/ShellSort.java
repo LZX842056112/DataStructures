3cd5a61cb2bbc8aefa63dfb847b7e1ddaaa1cafc
李宗效 <842056112@qq.com>
2020-02-15 20:47:07 +0800

    希尔排序 1.交换法 2.移位法

---
+++ src/com/atguigu/sort/ShellSort.java
@@ -0,0 +1,80 @@
+package com.atguigu.sort;
+
+import java.text.SimpleDateFormat;
+import java.util.Arrays;
+import java.util.Date;
+
+/**
+ * @Description
+ * @author liZongXiao
+ * @version
+ * @date 2020年2月15日下午7:50:14
+ * 希尔排序
+ * 1.交换法
+ * 2.移位法
+ */
+public class ShellSort {
+	
+	public static void main(String[] args) {
+//		int[] arr = { 8, 9, 1, 7, 2, 3, 5, 4, 6, 0 };
+		
+//		System.out.println("排序前");
+//		System.out.println(Arrays.toString(arr));
+		
+		int[] arr = new int[80000];
+		for (int i = 0; i < 80000; i++) {
+			arr[i] = (int) (Math.random() * 8000000); // 生成一个[0, 8000000) 数
+		}
+
+		Date data1 = new Date();
+		SimpleDateFormat simpleDateFormat = new SimpleDateFormat("yyyy-MM-dd HH:mm:ss");
+		String date1Str = simpleDateFormat.format(data1);
+		System.out.println("排序前的时间是=" + date1Str);
+		
+//		shellSort(arr);
+		shellSort2(arr);
+		
+		Date data2 = new Date();
+		String date2Str = simpleDateFormat.format(data2);
+		System.out.println("排序前的时间是=" + date2Str);
+		
+//		System.out.println("排序后");
+//		System.out.println(Arrays.toString(arr));
+	}
+
+	// 希尔排序，交换法
+	public static void shellSort(int[] arr) {
+		int temp = 0;
+		for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+			for (int i = gap; i < arr.length; i++) {
+				// 遍历各组中所有的元素(共gap组，每组有个元素), 步长gap
+				for (int j = i - gap; j >= 0; j -= gap) {
+					// 如果当前元素大于加上步长后的那个元素，说明交换
+					if (arr[j] > arr[j + gap]) {
+						temp = arr[j];
+						arr[j] = arr[j + gap];
+						arr[j + gap] = temp;
+					}
+				}
+			}
+		}
+	}
+	
+	// 希尔排序，移位法
+	public static void shellSort2(int[] arr) {
+		for (int gap = arr.length / 2; gap > 0; gap /= 2) {
+			for (int i = gap; i < arr.length; i++) {
+				int j = i;
+				int temp = arr[j];
+				if (arr[j] < arr[j - gap]) {
+					while (j - gap > 0 && temp < arr[j - gap]) {
+						arr[j] = arr[j - gap];
+						j -= gap;
+					}
+					//当退出while后，就给temp找到插入的位置
+					arr[j] = temp;
+				}
+			}
+		}
+	}
+}
