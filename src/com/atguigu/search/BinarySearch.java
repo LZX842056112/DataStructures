50d5e20ac00edec7fbd9282df788030d6cb9e9cb
李宗效 <842056112@qq.com>
2020-02-17 20:58:04 +0800

    完善二分查找算法（查找多个相同的数值）

--- src/com/atguigu/search/BinarySearch.java
+++ src/com/atguigu/search/BinarySearch.java
@@ -1,13 +1,28 @@
 package com.atguigu.search;
+
+import java.util.ArrayList;
+import java.util.List;
+
 /**
  * @Description
  * @author liZongXiao
  * @version
  * @date 2020年2月17日下午8:07:35
  * 二分查找算法
+ * 完善二分查找算法（查找多个相同的数值）
  * 注意：使用二分查找的前提是 该数组是有序的.
  */
 public class BinarySearch {
+	
+	public static void main(String[] args) {
+//		int arr[] = { 1, 8, 10, 89,1000,1000, 1234 };
+//		int resIndex = binarySearch(arr, 0, arr.length - 1, 1000);
+//		System.out.println("resIndex=" + resIndex);
+		
+		int arr[] = {1,2,3,4,5,6,7,8,9,10,10,10,11,12,13,14,15,16,17,18,19,20};
+		List<Integer> resIndexList = binarySearch2(arr, 0, arr.length - 1, 10);
+		System.out.println("resIndexList=" + resIndexList);
+	}
 	
 	/**
 	 * 
@@ -31,6 +46,46 @@ public class BinarySearch {
 			return binarySearch(arr, left, mid - 1, findVal);
 		}else {
 			return mid;
+		}
+	}
+	
+	//有多个相同的数值时，将所有的数值都查找到
+	public static List<Integer> binarySearch2(int[] arr, int left, int right, int findVal) {
+		// 当 left > right 时，说明递归整个数组，但是没有找到
+		if (left > right) {
+			return new ArrayList<Integer>();
+		}
+		int mid = (left + right) / 2;
+		int midVal = arr[mid];
+		
+		if (findVal > midVal) {//向右递归
+			return binarySearch2(arr, mid + 1, right, findVal);
+		}else if (findVal < midVal) {//向左递归
+			return binarySearch2(arr, left, mid - 1, findVal);
+		}else {
+			List<Integer> resIndexlist = new ArrayList<Integer>();
+			//向mid 索引值的左边扫描，将所有满足 1000， 的元素的下标，加入到集合ArrayList
+			int temp = mid - 1;
+			while (true) {
+				if (temp < 0 || arr[temp] != findVal) {
+					break;
+				}
+				resIndexlist.add(temp);
+				temp -= 1;
+			}
+			
+			resIndexlist.add(mid);
+			
+			//向mid 索引值的有边扫描，将所有满足 1000， 的元素的下标，加入到集合ArrayList
+			temp = mid + 1;
+			while (true) {
+				if (temp > arr.length - 1 || arr[temp] != findVal) {
+					break;
+				}
+				resIndexlist.add(temp);
+				temp += 1;
+			}
+			return resIndexlist;
 		}
 	}
 }
